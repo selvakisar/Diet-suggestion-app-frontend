5ba2b334d564a8f38dedc3f3295d5cdbe584b218
selvakisar <130025654+selvakisar@users.noreply.github.com>
2023-11-13 23:14:00 +0530

    auth complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes } from 'react-router';
+
+import BmiCalculator from './components/BMI';
+import Signup from './user/signup';
+import Login from './user/login';
+import Forget from './user/forget';
+import Reset from './user/reset';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+    <div>
+     
+    <Routes>
+    <Route path="/" element={<Login/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/forgetpass" element={<Forget/>}></Route>
+        <Route path="/resetpass/:token" element={<Reset/>}></Route>
+        <Route  path="/bmi" element= {<BmiCalculator/>} ></Route>
+
+    </Routes>
     </div>
+  </div>
   );
 }
 
